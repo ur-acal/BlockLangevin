f39d593458fa0d42a96ea51a87b2699852613362
Matt Burns <mburns13@ur.rochester.edu>
2025-03-31 15:43:35 -0400

    Added comments

--- block_langevin/bld.py
+++ block_langevin/bld.py
@@ -6,16 +6,21 @@ import pandas as pd
 from typing import Union, Iterable
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+
 def matrix_sqrt(matrix: torch.Tensor):
+    # Given a PSD matrix A, compute B such that A=BB
     vals, vecs = torch.linalg.eigh(matrix)
     matrix_sqrt = torch.matmul(vecs, torch.matmul(torch.diag(vals.real.sqrt()), torch.inverse(vecs)))
     return matrix_sqrt
 
 def gaussian_kl(gauss: D.MultivariateNormal, gauss2: D.MultivariateNormal):
+    # Compute the KL-divergence between two Gaussians
     return D.kl_divergence(gauss2, gauss).item()
        
 
 def gaussian_wasserstein(gauss: D.MultivariateNormal, gauss2: D.MultivariateNormal):
+    # Compute the 2-Wasserstein distance between two Gaussians
     cov = gauss.covariance_matrix
     mu = gauss.mean
     mu_emp = gauss2.mean
@@ -24,11 +29,12 @@ def gaussian_wasserstein(gauss: D.MultivariateNormal, gauss2: D.MultivariateNorm
     wasserstein = (mu - mu_emp).norm() + (
         cov + cov_emp - 2 * matrix_sqrt(sqrtcov @ cov_emp @ sqrtcov)
     ).trace()
-        # wasserstein_vals.append((i, wasserstein))
     return np.sqrt(wasserstein.item())
 
 class LDSampler:
-    def __init__(self, lr: float, thinning: int, beta: float, metric: str = "W2", sample_count=1):
+    """Implementation of the unadjusted Langevin algorithm using Euler-Maruyama discretization
+    """
+    def __init__(self, lr: float, thinning: int, beta: float, metric: str = "W2"):
         self.metric = metric
         self.thinning = thinning
         self.lr = lr
@@ -36,11 +42,15 @@ class LDSampler:
         self.coeff = np.sqrt(2 * lr / beta)
 
     def _log_sample(self, step: int):
+        # Log a single sample of the W2/KL-divergence, using the covariance/mean of the current state
         if step % self.thinning == 0:
+            # Compute the empirical mean/covariance across samples
             cov_emp = torch.cov(self.state)
             mu_emp = torch.mean(self.state, dim=1)
+            # Make a torch Distributions object
             gauss_emp = D.MultivariateNormal(loc=mu_emp, 
                                              covariance_matrix=cov_emp)
+            # Log the samples
             self.samples.append((step, D.kl_divergence(gauss_emp, self.target).item(), 
                                  gaussian_wasserstein(
                                      gauss_emp,
@@ -49,9 +59,10 @@ class LDSampler:
 
     def _do_step(self, 
                  gradient_oracle: callable):
+        # Perform a single time step
         self.state: torch.Tensor
         gradient = gradient_oracle(self.state)
-        # perform the Liemkuler-Matthews update
+        # perform the Euler-Maruyama update
         self.state += -self.lr * gradient + self.coeff * torch.randn_like(self.state)
 
     def sample(self, 
@@ -59,9 +70,10 @@ class LDSampler:
                nsteps: int,
                x0: torch.Tensor,
                target):
-        self.target = target
-        self.samples = []
-        self.state = x0.clone().detach()
+        # Main sampling loop
+        self.target = target # Target potential f(x)
+        self.samples = [] # sample storage
+        self.state = x0.clone().detach() # current state
         for i in tqdm(range(nsteps)):
             if i % self.thinning == 0:
                 self._log_sample(i)  
@@ -75,8 +87,19 @@ class BLDSampler:
                  beta: float, 
                  blocks: Union[int, Iterable],
                  metric: str = "W2", 
-                 selection: str = 'cyclic',
-                 sample_count=1):
+                 selection: str = 'cyclic'):
+        """Implementation of Block Langevin Dynamics
+
+        NOTE: Using an Euler-Maruyama discretization, higher-accuracy results should use a higher order method (e.g. SRK)
+
+        Args:
+            lr (float): Learning rate
+            thinning (int): thinning parameter (store a sample every `thinning` iterations)
+            beta (float): Inverse temperature
+            blocks (Union[int, Iterable]): Number of blocks
+            metric (str, optional): Metric to compute. Defaults to "W2".
+            selection (str, optional): Selection strategy, either "random" or "cyclic". Defaults to 'cyclic'.
+        """
         self.metric = metric
         self.thinning = thinning
         self.lr = lr
@@ -85,9 +108,14 @@ class BLDSampler:
         self.block_config = blocks
         self.coeff = np.sqrt(2 * lr / beta)
         self.current_block = None
-        self.sample_count = sample_count
 
     def _log_sample(self, step: int):
+        """Log a single sample of W2/KL vs. target distribution,
+          computed using the empirical mean/covariance
+
+        Args:
+            step (int): Current time step
+        """
         if step % self.thinning == 0:
             cov_emp = torch.cov(self.state)
             mu_emp = torch.mean(self.state, dim=1)
@@ -103,13 +131,19 @@ class BLDSampler:
 
     def _do_step(self, 
                  gradient_oracle: callable):
+        """Perform a time step single step
+
+        Args:
+            gradient_oracle (callable): Oracle for the gradient of the target potential $$f(x)$$
+        """
         self.state: torch.Tensor
         gradient = gradient_oracle(self.state)
-        # perform the Liemkuler-Matthews update
-        # breakpoint()
+        # perform the Euler-Maruyama update
         self.state += self.Ui.matmul(-self.lr * gradient + self.coeff * torch.randn_like(self.state))
 
     def change_block(self):
+        """Choose the next block to evolve
+        """
         if self.selection == 'random':
             self.current_block = np.random.randint(low=0, high=len(self.blocks))
         else:
@@ -117,6 +151,7 @@ class BLDSampler:
                 self.current_block = 0
             else:
                 self.current_block = (self.current_block + 1) % len(self.blocks)
+        # Compute the partial diagonal block matrix (U_ii=1 if i in current block, 0 otherwise)
         self.Ui.fill_(0)
         self.Ui[self.blocks[self.current_block], self.blocks[self.current_block]] = 1
  
@@ -126,7 +161,18 @@ class BLDSampler:
                cycles: int,
                block_steps: int,
                x0: torch.Tensor,
-               target):
+               target: D.Distribution):
+        """Main BLD sampling loop
+
+        Args:
+            gradient_oracle (callable): Oracle for potential gradient $$\nabla f(x)$$
+            cycles (int): Number of (expected) whole-problem passes
+            block_steps (int): Number of steps per block
+            x0 (torch.Tensor): Starting state (also determines number of replicas).
+                Expected shape is (d, R), where R is the number of replicas used for 
+                statistical estimation
+            target (torch.distributions.Distribution): Target distribution
+        """
         self.target = target
         if isinstance(self.block_config, int):
             self.blocks = np.array_split(np.arange(x0.shape[0]), self.block_config)
@@ -136,75 +182,93 @@ class BLDSampler:
         self.state = x0.clone().detach()
         self.Ui = torch.zeros((self.state.shape[0], self.state.shape[0]), device=device)
         lr = self.lr
-        # self.lr /= block_steps
-        # breakpoint()
+
         for i in tqdm(range(cycles * blocks)):
+            # Change blocks
             if i % block_steps == 0:
                 self.change_block()
-                # if i % (len(self.blocks) * block_steps) == 0:
+
+            # Log a new sample of W2/KL
             if i % self.thinning == 0:
                 self._log_sample(i) 
+            # Perform a time step
             self._do_step(gradient_oracle)
         self.lr = lr
     
 
-def make_df(samples, name, seed):
+def make_df(samples:Iterable[tuple], name: str, seed: int):
+    """Output a CSV based on samples
+
+    Args:
+        samples (Iterable[tuple]): List of records, with each record storing 
+            a time step, KL-divergence value, and a W2 value
+        name (str): Output file name (including .csv extension)
+        seed (int): Random seed used to collect data
+    """
     df = pd.DataFrame(data=samples, columns=['step', 'KL', 'W2'])
     df['seed'] = seed
     df.to_csv(f'data/{name}')
 
 
 if __name__ == '__main__':
-    N = 50
-    seed = 58888319060236145
+    N = 50 # Sample a 50 dimensional Gaussian
+    seed = 58888319060236145 # Arbitrary seed generated by torch, just here for future reproducibility
     torch.manual_seed(seed)
+    
+    # Make the target potential
+
     covmat = 5 * (torch.rand((N, N), device=device) * 2 - 1)
     covmat = (covmat.T + covmat) / 2
-    # covmat = torch.eye(N, device=device)
+    
+    # Ensure the covariance matrix is Symmetric + PD
     mineig = torch.linalg.eigvalsh(covmat).real.min()
     if mineig < 0:
         covmat += torch.eye(covmat.shape[0], device=device) * 1.2 * abs(mineig)
     simmat = covmat.inverse()
+    
+    # Make the potential function (assuming mean is 0)
     def grad(x):
         return simmat.matmul(x)
     x0 = 0.1*torch.randn((N, 10000), device=device)
+
+    # Make the target distribution object
     target = D.MultivariateNormal(loc=torch.zeros(N, device=device),
                                                    covariance_matrix=covmat)
+    
+    # Run for 150K steps with LR of 1e-3 (not magic numbers, but tractable to run and see differences)
     totalsteps = 150000
     lr = 1e-3
+
+    # Collect Full LD results (b=1)
     sampler = LDSampler(lr=lr, thinning=30, beta=1)
     sampler.sample(grad, totalsteps, x0, target=target)
     make_df(sampler.samples, name='full_gradient_gaussian.csv', seed=seed)
+
+
+    # Test the effect of blocks and block seletion on convergence
     for blocks in [2, 5, 10]:
         for epochs in [10]:
             for selection in ['random', 'cyclic']:
-                sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks, sample_count=500, selection=selection)
+                sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks, selection=selection)
                 sampler.sample(grad, totalsteps, epochs, x0, target=target)
                 make_df(sampler.samples, name=f'{selection}_block_b{blocks}_e{epochs}_gaussian.csv',
                          seed=seed)
-    for blocks in [2, 5, 10]:
-        for epochs in [10]:
-            # sampler = LDSampler(lr=1e-3, thinning=1, beta=1)
-            sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks, sample_count=500)
-            # sampler.sample(grad, 10000, x0, target=target)
-            sampler.sample(grad, totalsteps, epochs, x0, target=target)
-            make_df(sampler.samples, name=f'block_b{blocks}_e{epochs}_gaussian.csv', seed=seed)
+                
+    # Test the effect of epoch duration on convergence (e.g. annealing blocks for longer before swapping)
     for blocks in [5]:
         for epochs in [10, 25, 50]:
-            # sampler = LDSampler(lr=1e-3, thinning=1, beta=1)
-            sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks, sample_count=500)
-            # sampler.sample(grad, 10000, x0, target=target)
+            sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks)
             sampler.sample(grad, totalsteps, epochs, x0, target=target)
             make_df(sampler.samples, name=f'block_b{blocks}_e{epochs}_gaussian.csv', seed=seed)
+    
+    # Test the effect of fixed perturbations on convergence
     for blocks in [5]:
         epochs = 10
-        for perurbation in [0.6, .8, 1.0, 1.2]:
+        for perurbation in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]:
             perturb = (1 + perurbation * torch.randn(N, N, device=device)) * simmat
             def grad(x):
                 return perturb.matmul(x)
-            # sampler = LDSampler(lr=1e-3, thinning=1, beta=1)
-            sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks, sample_count=500)
-            # sampler.sample(grad, 10000, x0, target=target)
+            sampler = BLDSampler(lr=lr, thinning=30, beta=1, blocks=blocks)
             sampler.sample(grad, totalsteps, epochs, x0, target=target)
             make_df(sampler.samples, name=f'block_b{blocks}_e{epochs}_gaussian_p_{perurbation}.csv', seed=seed)
 
